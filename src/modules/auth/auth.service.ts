62d32c9220755d4dcc514f4876a830357915b99e
dev-vedant <yvedant13@gmail.com>
2025-09-24 11:50:08 +0530

    check: path change narrow for csrfToken

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -93,7 +93,7 @@ export const setCookieService = (res: Response, refreshToken: string, csrfToken:
         httpOnly: false,    
         secure: true,       // for https
         sameSite: "none",
-        path: '/',
+        path: '/api/auth/refresh-token',
         maxAge: config.jwt.refreshExpiresIn
     });
 }
